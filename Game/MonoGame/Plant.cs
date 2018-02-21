fbe2ab70ccb68e01a7141491cb1c9917bfb589f7
Bart0110 <Bart0110@users.noreply.github.com>
2018-02-21 16:23:40 +0100

    Create plant object

--- Game/MonoGame/Plant.cs
+++ Game/MonoGame/Plant.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace MonoGame
 {
-    public abstract class Plant
+    public class Plant
     {
+        public static readonly Plant ANANAS = new Plant("Ananas", 1000);
+
+        public static IEnumerable<Plant> Values
+        {
+            get { yield return ANANAS; }
+        }
+
+        public string Name { get; }
+        public int GrowTime { get; }
+        public Texture2D Texture { get; private set; }
+
+        public Plant(string name, int growTime)
+        {
+            this.Name = name;
+            this.GrowTime = growTime;
+        }
+
+        public void LoadContent(ContentManager content, string path)
+        {
+            Texture = content.Load<Texture2D>(path);
+        }
 
     }
 }
