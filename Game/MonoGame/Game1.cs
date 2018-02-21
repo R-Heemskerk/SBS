d878bea4b58d66e61450e949013451320478b2c0
Suzanneinformatica <35996915+Suzanneinformatica@users.noreply.github.com>
2018-02-21 11:29:49 +0100

    TEST

--- Game/MonoGame/Game1.cs
+++ Game/MonoGame/Game1.cs
@@ -2,7 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace MonoGame
+namespace MonoGame                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 {
     /// <summary>
     /// This is the main type for your game.
