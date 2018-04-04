e55212789eda27b3bce6ea92b398beb313ef6c30
Bart0110 <Bart0110@users.noreply.github.com>
2018-04-04 21:41:46 +0200

    Centralize strings

---
+++ Game/MonoGame/Strings.cs
@@ -0,0 +1,10 @@
+﻿namespace MonoGame
+{
+    public static class Strings
+    {
+        public static string ErrorHarvestTitle = "De plant is nog niet volgroeid!",
+            ErrorHarvestBody = "Je kan deze plant nog niet oogsten want deze plant is nog niet volgroeid.\nWacht nog even totdat de vrucht tot uiting komt.",
+            ErrorNotEnoughSeedsTitle = "Je hebt niet genoeg zaadjes!",
+            ErrorNotEnoughSeedsBody = "Je hebt niet genoeg %%PLANT zaadjes.\n\nJe kan meer zaadjes kopen in de winkel\ndoor rechts onderin op het mandje te klikken.";
+    }
+}
